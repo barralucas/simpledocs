c8f5b1ed671ea8cb10843b77f050896331595064
Lucas Barra <lucas.trindade@funcionarios.automatizai.com.br>
2024-07-23 21:01:12 +0000

    feat: authentication system with clerk

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -1,6 +1,6 @@
 import { Editor } from '@/components/editor/Editor';
 import Header from '@/components/Header';
-import React from 'react';
+import { SignedIn, SignedOut, SignInButton, UserButton } from '@clerk/nextjs';
 
 const Document = () => {
   return (
@@ -9,6 +9,13 @@ const Document = () => {
         <div className='flex w-fit items-center justify-center gap-2'>
           <p className='document-title'>This is a fake document title</p>
         </div>
+
+        <SignedOut>
+          <SignInButton />
+        </SignedOut>
+        <SignedIn>
+          <UserButton />
+        </SignedIn>
       </Header>
       <Editor />
     </div>
