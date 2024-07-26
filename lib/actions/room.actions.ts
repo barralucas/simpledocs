126ee94853ed183188c7947f0dd5ccd09e519ece
Lucas Barra <lucas.trindade@funcionarios.automatizai.com.br>
2024-07-26 22:07:13 +0000

    feat: list all documents on profile

--- lib/actions/room.actions.ts
+++ lib/actions/room.actions.ts
@@ -63,3 +63,13 @@ export const updateDocument = async (roomId: string, title: string) => {
         console.log(`Error updating document: ${error}`);
     }
 }
+
+export const getDocuments = async (email: string) => {
+    try {
+        const rooms = await liveblocks.getRooms({ userId: email });
+
+        return parseStringify(rooms);
+    } catch (error) {
+        console.log(`Error fetching documents: ${error}`);
+    }
+}
