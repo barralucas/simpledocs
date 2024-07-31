076e8c297887c8198b83d9dbce7ee862e0a63d36
Lucas Barra <lucas.trindade@funcionarios.automatizai.com.br>
2024-07-31 17:57:44 +0000

    feat: share modal

--- components/CollaborativeRoom.tsx
+++ components/CollaborativeRoom.tsx
@@ -10,6 +10,8 @@ import { Input } from "./ui/input";
 import Image from "next/image";
 import { updateDocument } from "@/lib/actions/room.actions";
 import Loader from "./Loader";
+import { Share } from "next/font/google";
+import ShareModal from "./ShareModal";
 
 const CollaborativeRoom = ({ roomId, roomMetadata, users, currentUserType }: CollaborativeRoomProps) => {
     // const currentUserType = 'editor'; // TODO: get user type from context
@@ -102,6 +104,14 @@ const CollaborativeRoom = ({ roomId, roomMetadata, users, currentUserType }: Col
 
                         <div className="flex w-full flex-1 justify-end gap-2 sm:gap-3">
                             <ActiveCollaborators />
+
+                            <ShareModal
+                            roomId={roomId}
+                            collaborators={users}
+                            creatorId={roomMetadata.creatorId}
+                            currentUserType={currentUserType}
+                            />
+
                             <SignedOut>
                                 <SignInButton />
                             </SignedOut>
